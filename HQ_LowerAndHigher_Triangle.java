7467c883806ee98ddfeb7adc05cfb2a72122576d
Lucas Tamura <lucasestvez007@gmail.com>
2023-05-25 17:32:11 -0300

    HQ

--- HQ_LowerAndHigher_Triangle.java
+++ HQ_LowerAndHigher_Triangle.java
@@ -17,17 +17,16 @@ public class HQ_LowerAndHigher_Triangle {
         System.out.println();
 
         for (int i = 0; i < 8; i++) {
-            for (int j = (i+1); j < 8; j++) {
-                hq2[i][j] = 1;
-            }
-        }
-        for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-               if(i==j){
-                   hq2[i][j] = 1;
-               }
+                for (int k = (i+1); k < 8 ; k++) {
+                    hq2[i][k] = 1;
+                }
+                if(i==j){
+                    hq2[i][j] = 1;
+                }
             }
         }
+
         System.out.println("Higher: ");
         for (int[] i : hq2)
             System.out.println(Arrays.toString(i));
